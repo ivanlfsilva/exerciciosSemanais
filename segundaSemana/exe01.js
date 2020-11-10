db9e3ebf7664c32ead3707531219f3e5d937632d
ivanlfsilva <ivanlfsilva@ufrn.edu.br>
2020-11-09 21:32:32 -0300

    respostaExe

---
+++ segundaSemana/exe01.js
@@ -0,0 +1,10 @@
+
+function executarExercicio() {
+    var a = Number(document.getElementById('a').value);
+    var b = Number(document.getElementById('b').value);
+    var c = Number(document.getElementById('c').value);
+    //var d = Number(document.getElementById('d').value);
+    // implemente a partir daqui
+    var r = - a - (b + (c * a * b)) / (c * a); // atribua o resultado dos seus algoritmos a r
+    document.getElementById('resultado').innerHTML = "O resultado Da Expressão é : " + r;
+    }
